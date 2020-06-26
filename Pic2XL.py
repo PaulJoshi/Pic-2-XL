e482c97d44795d849e1fb6861e9e9eea811fc36c
Paul Joshi <49583322+PaulJoshi@users.noreply.github.com>
2020-06-26 20:31:07 +0530

    1. Now column width is set to 3 2. Image to be processed can be passed as command line argument 3. Improves documentation and logs

--- Pic2XL.py
+++ Pic2XL.py
@@ -1,9 +1,14 @@
 from PIL import Image
 from openpyxl import Workbook
 from openpyxl.styles import Color, PatternFill, Font, Border
+from openpyxl.utils import get_column_letter
+from sys import argv
 
 
-im = Image.open('testImg.jpg', 'r')
+try:
+    im = Image.open(str(argv[1]), 'r')
+except:
+    im = Image.open('testImg.jpg', 'r')
 pix_val = list(im.getdata())
 pix_val_flat = [x for sets in pix_val for x in sets]
 
@@ -18,12 +23,15 @@ def main():
     for x in range(1,im.size[1] + 1):
         print("row no: " + str(x))
         for y in range(1,im.size[0] + 1):
-            print("column no: " + str(y))
+            print("row no: " + str(x) + " | column no: " + str(y))
             cell_color = rgb2hex(pix_val_flat[count], pix_val_flat[count + 1], pix_val_flat[count + 2])
             ws.cell(row=x, column=y).fill = PatternFill(start_color=cell_color, end_color=cell_color, fill_type = 'solid')
             count += 3
-    wb.save('image.xlsx')
 
+    for col in range(1,im.size[0] + 1):                 #Sets column width to 3
+        ws.column_dimensions[get_column_letter(col)].width = 3
+
+    wb.save('image.xlsx')                               #Saves workbook into .xlsx format
 
 
 if __name__ == "__main__":
